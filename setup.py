17efe3adf187ef097e0896ca1b468ae3cea5a56a
John Fernandez <jafernandez.tampabay@gmail.com>
2023-08-16 22:37:49 -0400

    just hack it

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ this_directory = Path(__file__).parent
 print(this_directory)
 print(this_directory / "README.rst")
 
-long_description = (this_directory / "README.rst").read_text()
+long_description = ("/home/runner/work/django_cityloc_pkg_johnafernandez" / "README.rst").read_text()
 
 setup(
     long_description=long_description
