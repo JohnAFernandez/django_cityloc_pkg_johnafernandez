71397003ac9c2bfd56f584dcd21452be9e3c5d70
John Fernandez <jafernandez.tampabay@gmail.com>
2023-08-16 22:32:59 -0400

    for troubleshooting

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 # put the contents of your README file into the long_description
 from pathlib import Path
 this_directory = Path(__file__).parent
+
+print(this_directory)
+print(this_directory / "README.rst")
+
 long_description = (this_directory / "README.rst").read_text()
 
 setup(
